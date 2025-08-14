93ada6393c1ea3d5269292a67d1be9eeba021c81
Chayan Roy <tochayanroy@gmail.com>
2025-08-14 21:58:46 +0530

    Token and hash Password

--- ecommerceBackend/index.js
+++ ecommerceBackend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const passport = require('passport')
+
 const dotenv = require('dotenv');
 
+
 const db = require('./config/db');
+require('./middleware/passport-config');
 
 const userRoutes = require('./routes/userRoutes')
 dotenv.config();
 app.use(express.json())
+app.use(passport.initialize());
 
 app.use('/user', userRoutes)
 
--- ecommerceBackend/routes/userRoutes.js
+++ ecommerceBackend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 const User = require('../modules/userSchema')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
 
 
 
@@ -15,15 +17,23 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ message: "User already exist" })
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt)
+
         user = new User({
             userName,
             phoneNumber,
             email,
-            password
+            password: hashPassword
         })
 
         await user.save();
-        res.status(200).json({ message: "Account created Successfully" })
+
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN_SECRET_KEY)
+
+
+        res.status(200).json({ message: "Account created Successfully", token })
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Server Error" })
@@ -44,11 +54,15 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ message: "User not exist" })
         }
 
-        if (user.password != password) {
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
             return res.status(400).json({ message: "User not exist" })
         }
 
-        res.status(200).json({ message: "Login Successfully" })
+        const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN_SECRET_KEY)
+
+        res.status(200).json({ message: "Login Successfully", token })
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Server Error" })
@@ -59,19 +73,10 @@ router.post('/login', async (req, res) => {
 
 
 
-router.get('/profile', async (req, res) => {
+router.get('/profile', passport.authenticate('jwt', {session:false}), async (req, res) => {
     try {
-        const { email, password } = req.body;
 
-        let user = await User.findOne({ email });
-
-        if (!user) {
-            return res.status(400).json({ message: "User not exist" })
-        }
-
-        if (user.password != password) {
-            return res.status(400).json({ message: "User not exist" })
-        }
+        const user = await User.findById(req.user.id).select('-password')
 
         res.status(200).json({ user })
     } catch (err) {
